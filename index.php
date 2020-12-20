fdf39cf9f27379171f30b52175eb54e98277da58
alikhaled17 <ka99ali@gmail.com>
2020-12-20 02:35:43 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Car Tech</title>
+    <!-- my css files -->    
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css" />
+
+    <script src="js/responde.js"></script>    
+</head>
+<body>
+    
+
+    <!-- Start Upper Bar -->
+    <div class="upper-bar">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <i class="fa fa-phone"></i><span> +20 111 2332 199</span>,
+                    <i class="fa fa-envelope-o"></i> CarTech@gmail.com
+                </div>
+                <div class="col-sm text-right">
+                    <span>Let's Work Together! </span>
+                    <span class="get-quote">Get Qoute</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Upper Bar -->
+    <!-- Start Nav Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" data-wow-duration='.5s' href="#">
+                <img class="wow wobble" src="imgs/logo1.png" alt="carella" width="120px">
+                <span>Car</span><span>Tech</span>
+            </a>
+          
+        
+            <div class="collapse navbar-collapse" id="ournavbar">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact US</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false">
+                            Settings
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item " href="#">Logout</a>
+                            <a class="dropdown-item " href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item " href="#">Logout</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="image-header row">
+        <div class="container">
+            <div class="right-side col-6">
+                <h1>Carella</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam aspernatur amet animi vero tempora, odit esse non eos dignissimos ipsa possimus alias, necessitatibus molestias, error cumque delectus omnis ratione eaque.
+                </p>
+            </div>
+            <div class="left-side col-6">
+                <img src="imgs/home-img.png" alt="home-img" width="100%">
+            </div>
+        </div>
+    </div>
+ 
+    
+    
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script>new WOW().init();</script>    
+    <script src="js/index.js"></script>
+</body>
+
+</html>
