352b79682d12c2fe8b1924c1b143e11ecb8db3d5
alikhaled17 <ka99ali@gmail.com>
2020-12-21 14:39:01 +0200

    add sign up

---
+++ src/signup.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Car Tech - Contact</title>
+    <!-- my css files -->
+    <link rel="stylesheet" href="../css/bootstrap.css" />
+    <link rel="stylesheet" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="../css/animate.css"/>
+    <link rel="stylesheet" href="../css/style.css"/>
+    <link rel="stylesheet" href="../css/services.css"/>
+
+    <script src="../js/responde.js"></script>
+</head>
+<body>
+    
+    <?php include('../header.php'); ?>
+    
+    <div class="signup">
+        <div class="container">
+            <div class="row">
+                <div class="left-side">
+                    <!-- form -->
+                </div>
+                <div class="right-side">
+                    <div class="sign-img">
+                        <div class="shape"></div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/wow.min.js"></script>
+    <script>new WOW().init();</script>
+    <script src="../js/script.js"></script>
+</body>
+</html>
