269c020d89d3f82eac7d308db0a94820d1b12844
alikhaled17 <ka99ali@gmail.com>
2020-12-30 00:39:33 +0200

    fix update qeuery for location

--- src/get_loc.php
+++ src/get_loc.php
@@ -9,7 +9,7 @@
     $cords="SELECT * FROM coordinates WHERE p_id = '$id' ";
     $ifcords = mysqli_query($conn, $cords);
     if($ex = mysqli_num_rows($ifcords)) {
-        $conn->query("UPDATE coordinates SET longitude= '$lon' , latitude = '$lat' , p_id='$id' ");
+        $conn->query("UPDATE coordinates SET longitude= '$lon' , latitude = '$lat' WHERE p_id = '$id' ");
     } 
     else {
         $conn->query("INSERT INTO coordinates (longitude, latitude, p_id) 
