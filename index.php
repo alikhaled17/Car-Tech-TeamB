05dff4f6a826bbb052d1e19fff198a1c600175b0
Mayar Arabi <mayararaby246@gmail.com>
2021-01-03 12:19:45 -0800

    Home updated

--- index.php
+++ index.php
@@ -37,24 +37,6 @@ session_start();
             </div>
         </div>
     </div>
-    <div class="about-us row">
-        <div class="container">
-            <div class="left-side col-2">
-                <h2 class="aboutH2">About us</h2>
-            </div>
-            <div class="right-side col-10">
-                <div></div>
-                <p>
-                Our expertise with various vehicles, automotive technologies is as diverse as the people .
-                We believe in the power of ordinary people to do extra ordinary things.
-                We employ our collective potential to vastly improve your automotive service experience.
-                We all have one thing in common…Passion for the automobile.
-            </p>
-            </div>
-        </div>
-    </div>
-
-
     <div class="our-sers row">
         <div class="container">
             <div class="upper">
@@ -106,41 +88,7 @@ session_start();
                 </div>
             </div>
         </div>
-    </div>
-
-
-
-
-    <div id="port" class="our-team row">
-        <div class="container">
-            <h2>Our Team </h2>
-            <p>Our team is your team. When your mission is to be better, faster and smarter, you need the best people driving your vision forward , Get to know some of us.</p>
-            <div class="box">
-                <div class="member wow bounceInUp">
-                    <div class="overlay">Ali</div>
-                    <img src="imgs/ali_k.jpg" alt="">
-                </div>
-                <div class="member wow bounceInUp">
-                    <div class="overlay">Mayar</div>                    
-                    <img src="imgs/mayar_a.jpg" alt="">
-                </div>
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Mahmoud</div>                    
-                    <img src="imgs/mahmoud_Y.jpg" alt="">
-                </div>
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Turki</div>
-                    <img src="imgs/ahmed_T.jpg" alt="">
-                </div>
-                
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Sally</div>                    
-                    <img src="imgs/sally_E.jpg" alt="">
-                </div>  
-            </div>
-        </div>
-    </div>
-    
+    </div>    
     
     <?php include('footer.php'); ?>
 
