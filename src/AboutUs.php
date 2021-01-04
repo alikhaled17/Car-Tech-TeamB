24826aba822c4a4540052451c8ad3a3366ad6286
alikhaled17 <ka99ali@gmail.com>
2021-01-04 02:33:10 +0200

    home style and fix services

--- src/AboutUs.php
+++ src/AboutUs.php
@@ -16,7 +16,7 @@ session_start();
     <link rel="stylesheet" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/animate.css">
     <link rel="stylesheet" href="../css/style.css" />
-    <link rel="stylesheet" href="../css/home.css" />
+    <link rel="stylesheet" href="../css/about.css" />
 </head>
 <body>
     <?php include('../header.php'); ?>
@@ -27,48 +27,94 @@ session_start();
             <div class="left-side col-2">
                 <h2 class="aboutH2">About us</h2>
             </div>
-            <div class="right-side col-10">
-                <div></div>
+            <div class="right-side col-12">
                 <p>
-                Our expertise with various vehicles, automotive technologies is as diverse as the people .
-                We believe in the power of ordinary people to do extra ordinary things.
-                We employ our collective potential to vastly improve your automotive service experience.
-                We all have one thing in common .. Passion for the automobile.
-            </p>
+                    Our expertise with various vehicles, automotive technologies is as diverse as the people .
+                    We believe in the power of ordinary people to do extra ordinary things.
+                    We employ our collective potential to vastly improve your automotive service experience.
+                    We all have one thing in common .. Passion for the automobile.
+                </p>
             </div>
         </div>
     </div>
 
-    <div id="port" class="our-team row">
-        <div class="container">
-            <h2>Our Team </h2>
-            <p>Our team is your team. When your mission is to be better, faster and smarter, you need the best people driving your vision forward , Get to know some of us.</p>
-            <div class="box">
-                <div class="member wow bounceInUp">
-                    <div class="overlay">Ali</div>
-                    <img src="../imgs/ali_k.jpg" alt="">
-                </div>
-                <div class="member wow bounceInUp">
-                    <div class="overlay">Mayar</div>                    
-                    <img src="../imgs/mayar_a.jpg" alt="">
-                </div>
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Mahmoud</div>                    
-                    <img src="../imgs/mahmoud_Y.jpg" alt="">
-                </div>
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Turki</div>
-                    <img src="../imgs/ahmed_T.jpg" alt="">
-                </div>
-                
-                <div class="member wow bounceInDown">
-                    <div class="overlay">Sally</div>                    
-                    <img src="../imgs/sally_E.jpg" alt="">
-                </div>  
-            </div>
-        </div>
+    <div class="container">
+        <h2 class="h2_team">
+            - Our Team
+        </h2>
     </div>
-    
+    <div class="container">
+    <div class="row">
+        
+        <div class="our-team">
+            <div class="picture">
+            <img class="img-fluid" src="../imgs/ali_K.jpg">
+            </div>
+            <div class="team-content">
+            <h4 class="name">Ali khaled</h4>
+            <h5 class="title">Web Developer</h5>
+            </div>
+            <ul class="social">
+            <li><a href="https://github.com/alikhaled17" class="fa fa-github" aria-hidden="true"></a></li>
+            <li><a href="https://www.linkedin.com/in/ali-khaled-7a4a981b4/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+            </ul>
+        </div>
+        <div class="our-team">
+            <div class="picture">
+            <img class="img-fluid" src="../imgs/mayar_a.jpg">
+            </div>
+            <div class="team-content">
+            <h4 class="name">Mayar Araby</h4>
+            <h5 class="title">Web Developer</h5>
+            </div>
+            <ul class="social">
+            <li><a href="https://github.com/mayararaby" class="fa fa-github" aria-hidden="true"></a></li>
+            <li><a href="https://www.linkedin.com/in/mayar-araby-20b671127/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+            </ul>
+        </div>
+        <div class="our-team">
+            <div class="picture">
+            <img class="img-fluid" src="../imgs/mahmoud_Y.jpg">
+            </div>
+            <div class="team-content">
+            <h4 class="name">Mahmoud Elsharawy</h4>
+            <h5 class="title">Web Developer</h5>
+            </div>
+            <ul class="social">
+            <li><a href="https://github.com/MahmoudElsh3rawy" class="fa fa-github" aria-hidden="true"></a></li>
+            <li><a href="https://www.linkedin.com/in/mahmoud-elsharawy-266aa4136/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+            </ul>
+        </div>
+        <div class="our-team">
+            <div class="picture">
+            <img class="img-fluid" src="../imgs/ahmed_T.jpg">
+            </div>
+            <div class="team-content">
+            <h4 class="name">Ahmd Turky</h4>
+            <h5 class="title">Web Developer</h5>
+            </div>
+            <ul class="social">
+            <li><a href="https://github.com/AhmedTurkiii" class="fa fa-github" aria-hidden="true"></a></li>
+            <li><a href="https://www.linkedin.com/in/ahmed-turki-6a8062120/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+            </ul>
+        </div>
+        <div class="our-team">
+            <div class="picture">
+            <img class="img-fluid" src="../imgs/sally_E.jpg">
+            </div>
+            <div class="team-content">
+            <h4 class="name">Sally Eissawy</h4>
+            <h5 class="title">Web Developer</h5>
+            </div>
+            <ul class="social">
+            <li><a href="https://github.com/sallyElsayedShehataa" class="fa fa-github" aria-hidden="true"></a></li>
+            <!-- <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a></li> -->
+            <li><a href="https://www.linkedin.com/" class="fa fa-linkedin" aria-hidden="true"></a></li>
+            </ul>
+        </div>
+       
+    </div>
+    </div>
     
     <?php include('../footer.php'); ?>
 
