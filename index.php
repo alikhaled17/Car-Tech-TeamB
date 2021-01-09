b7a74ca057ebb8a12efb1943630d8381cb9eb621
Mayar Arabi <mayararaby246@gmail.com>
2021-01-09 11:59:49 -0800

    add script tag in the head of page

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Car Tech</title>
-    <!-- my css files -->    
+    <!-- my css files -->  
+      
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/animate.css">
@@ -22,6 +23,8 @@ session_start();
     
 </head>
 <body>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <?php include('header.php'); ?>
     <?php include('Config.php'); ?>
 
@@ -193,8 +196,7 @@ session_start();
     
     <?php include('footer.php'); ?>
 
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+
     <script src="js/wow.min.js"></script>
     <script>new WOW().init();</script>  
     <script src="js/script.js"></script>
