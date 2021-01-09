fc3372459b0d5bf78d536aaacf3f46f0ec410a5e
alikhaled17 <ka99ali@gmail.com>
2021-01-09 03:53:46 +0200

    database remote and deploy

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $('.navar li').each(function () {
-        if(window.location.pathname == $(this).children().attr("href") || $(this).children().attr("href") === '#' )
-        {
+        if (window.location.pathname == "https://care-tech.herokuapp.com" + $(this).children().attr("href") || $(this).children().attr("href") === '#') {
             console.log(window.location.pathname);
             console.log("-- ", $(this).children().attr("href"));
             $(this).addClass('active').siblings().removeClass('active');
