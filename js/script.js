6867a8d163ef90969195088c216a9fddd8393654
alikhaled17 <ka99ali@gmail.com>
2021-01-09 23:00:13 +0200

    fix header color

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
             console.log("path: ", "https://car-tch.herokuapp.com");
             console.log("href: ", $(this).children().attr("href"));
             console.log("sum: ", "https://car-tch.herokuapp.com", window.location.pathname);
-            // $(this).addClass('active').siblings().removeClass('active');
+            $(this).addClass('active').siblings().removeClass('active');
             return false;
         } else {
             console.log("not found");
