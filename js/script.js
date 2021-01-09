ba386b2d43232db88f6d10d4c4a3acb2fc8b14dc
alikhaled17 <ka99ali@gmail.com>
2021-01-09 22:31:52 +0200

    commit descriptive

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
     $('.navar li').each(function () {
-        if ("https://car-tch.herokuapp.com"+ window.location.pathname == + $(this).children().attr("href") || $(this).children().attr("href") === '#') {
-            console.log(window.location.pathname);
-            console.log("-- ", $(this).children().attr("href"));
-            $(this).addClass('active').siblings().removeClass('active');
+        if ("https://car-tch.herokuapp.com" + window.location.pathname == + $(this).children().attr("href") || $(this).children().attr("href") === '#') {
+            console.log("path: ", window.location.pathname);
+            console.log("href: ", $(this).children().attr("href"));
+            console.log("sum: ", window.location.pathname, $(this).children().attr("href"));
+            // $(this).addClass('active').siblings().removeClass('active');
             return false;
+        } else {
+            console.log("path: ", window.location.pathname);
+            console.log("href: ", $(this).children().attr("href"));
+            console.log("sum: ", window.location.pathname, $(this).children().attr("href"));
         }
     })
 });
