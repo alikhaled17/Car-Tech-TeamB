078c85868fb75bb7e798fe17375f6efbe3bdc68f
alikhaled17 <ka99ali@gmail.com>
2021-01-09 02:36:24 +0200

    database remote and deploy

--- Config.php
+++ Config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "cartech";
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "bf15ade353421d";
+$password = "68f8e492";
+$dbname = "heroku_031f0ce2cceb50d";
 
 
 // Create connection
