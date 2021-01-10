fa00aca5a5d78ea68a09e1b49fd54e327c08d440
Mayar Arabi <mayararaby246@gmail.com>
2021-01-10 15:25:57 -0800

    Try to fix pagination in search submit

--- src/search_submit.php
+++ src/search_submit.php
@@ -93,7 +93,7 @@
                 $total_pages = ceil($total_records / $limit); 
                 $pagLink = "<ul class='pagination'>";  
                 for ($i=1; $i<=$total_pages; $i++) {
-                            $pagLink .= "<li class='page-item'><a class='page-link' href='pagination.php?page=".$i."'>".$i."</a></li>";	
+                    $pagLink .= "<li class='page-item'><a class='page-link' href='pagination.php?page=".$i."'>".$i."</a></li>";	
                 }
                 echo $pagLink . "</ul>";  
             } else {
