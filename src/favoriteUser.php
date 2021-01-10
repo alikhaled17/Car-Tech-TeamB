3d412a2c801f921fff7d2d2da5f8688f0a0a9892
Mayar Arabi <mayararaby246@gmail.com>
2021-01-10 08:05:03 -0800

    edit in style of char

--- src/favoriteUser.php
+++ src/favoriteUser.php
@@ -59,7 +59,7 @@ session_start();
                                         echo ('
                                                 <li class="result-card">
                                                 <div class="lift"> ');
-                                        echo ('<a class="charimge"  target="_blank" href="visitProvider.php?id='.$provider['id'].' ">'. strtoupper(substr($provider['username'], 0, 1)) .'</a>');     
+                                        echo ('<div><a class="charimge"  target="_blank" href="visitProvider.php?id='.$provider['id'].' ">'. strtoupper(substr($provider['username'], 0, 1)) .'</a></div>');     
                                         echo   ('</div>'.
                                                 '<div class="name-hover">
                                                     <h6>'.$provider['username'].'</h6>
