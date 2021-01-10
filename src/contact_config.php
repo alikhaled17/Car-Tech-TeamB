54c2f46a3117ddab57a8e830834072b2010c26e2
MahmoudElsh3rawy <mahmoudelsharawy92@gmail.com>
2021-01-10 21:47:24 +0200

    Add File Mail.php

--- src/contact_config.php
+++ src/contact_config.php
@@ -1,6 +1,6 @@
 <?php
 if (isset ($_POST['send'])) {
-    require_once "../Mail.php";
+    require_once "Mail.php";
     $username = 'info.cartechb@gmail.com';
     $password = 'car5857507M';
     $smtpHost = 'ssl://smtp.gmail.com';
