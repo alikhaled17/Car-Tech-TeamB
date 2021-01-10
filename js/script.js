64bf2ab850d974a2107441e75ec1691935f06cb3
alikhaled17 <ka99ali@gmail.com>
2021-01-10 04:43:39 +0200

    fix header color

--- js/script.js
+++ js/script.js
@@ -1,37 +1,17 @@
-// window.onload = (event) => {
-//     var slides = document.getElementsByClassName("nav-item");
-//     for (var i = 0; i < slides.length; i++) {
-//         if($("#myDiv").length) {
-
-//         }
-//         var x = slides.item(i).firstElementChild.getAttribute('href');
-//         console.log(x);
-
-//     }
-// }
 
 $(document).ready(function () {
     $('.navar li').each(function () {
         if ($(this).children().length) {
             if (($(this).children().attr("href")).indexOf(window.location.pathname) != -1 || $(this).children().attr("href") === '#') {
-                console.log("exist");
-                console.log("path: ", "https://car-tch.herokuapp.com");
-                console.log("href: ", $(this).children().attr("href"));
-                console.log("sum: ", ("https://car-tch.herokuapp.com" + window.location.pathname));
                 $(this).addClass('active').siblings().removeClass('active');
-                // $(this).data('tousername')
                 return false;
             } else {
-                console.log("not found");
-                console.log("path: ", "https://car-tch.herokuapp.com");
-                console.log("href: ", $(this).children().attr("href"));
-                console.log("sum: ", ("https://car-tch.herokuapp.com" + window.location.pathname));
+                $('.navar li').removeClass('active');
             }
         } else {
             console.log("else");
             return;
         }
-
     })
 });
 
