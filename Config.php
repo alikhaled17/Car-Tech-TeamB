c60330249094e3a53a391f79bafda8b630508ff4
alikhaled17 <ka99ali@gmail.com>
2021-01-11 20:28:06 +0200

    fix local

--- Config.php
+++ Config.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "us-cdbr-east-02.cleardb.com";
-// $username = "b541e9d2344cba";
-// $password = "84bd742c";
-// $dbname = "heroku_14b5ff014b7ea68";
+$servername = "us-cdbr-east-02.cleardb.com";
+$username = "b541e9d2344cba";
+$password = "84bd742c";
+$dbname = "heroku_14b5ff014b7ea68";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "cartech1";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "cartech1";
 
 
 
