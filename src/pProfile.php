970e7f63272ea05bc962211cb99c7809b2c3726f
alikhaled17 <ka99ali@gmail.com>
2021-01-11 23:57:09 +0200

    fix local

--- src/pProfile.php
+++ src/pProfile.php
@@ -1,13 +1,22 @@
 <?php 
     include('../Config.php');  
     session_start();
-    session_regenerate_id();
+    
+    
+    if(isset($_POST['reg'])) {
+        $query = "
+        UPDATE p_address
+        SET region_id  = '".$_POST["reg"]."', street = '". $_POST["street"] ."'
+        WHERE p_id = '".$_POST["id"]."'
+        ";
 
-    if(!isset($_SESSION['p_id']))      
-    {
-        header("Location:login.php");
+        echo $_POST["reg"];
+        $conn->query($query);
     }
+    
+
     $id = $_SESSION['p_id'];
+   
 
     $sql="SELECT users.*,
         providers.comm_img,
@@ -24,13 +33,58 @@
         inner join p_address on p_address.p_id = providers.user_id)
         inner join regions on regions.id = p_address.region_id)
         inner join cities on cities.id = regions.city_id )
-        WHERE users.id = '$id'
-        GROUP BY users.id
-        ";
+        WHERE
+            users.id = '$id'";
 
     $result = mysqli_query($conn, $sql);
     $rows = mysqli_num_rows($result);
     $user_data = mysqli_fetch_array($result);
+
+
+    if(isset($_POST['update']))
+    { 
+        if (
+                isset($_POST['username'])
+                && isset($_POST['email'])
+                && isset($_POST['pass'])
+                && isset($_POST['phone'])
+                && (isset($_POST['Gas']) || isset($_POST['Wash'])
+                || isset($_POST['Maintenance']) || isset($_POST['Trailer']))
+        )
+        {
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = $_POST['pass'];
+            $phone = $_POST['phone'];
+
+            $result = mysqli_query($conn, "UPDATE users SET username='$username',email='$email',
+            password='$password',phone='$phone' WHERE id=$id");  
+            
+            $conn->query("DELETE FROM prov_services WHERE p_id = $id");
+
+            if (isset($_POST['Gas'])) {
+                $conn->query("INSERT INTO prov_services (p_id,ser_id) VALUES ('$id','10')");
+            }
+            if (isset($_POST['Wash'])) {
+                $conn->query("INSERT INTO prov_services (p_id,ser_id) VALUES ('$id','20') ");
+            }
+            if (isset($_POST['Maintenance'])) {
+                $conn->query("INSERT INTO prov_services (p_id,ser_id) VALUES ('$id','30')");
+            }
+            if (isset($_POST['Trailer'])) {
+                $conn->query("INSERT INTO prov_services (p_id,ser_id) VALUES ('$id','40') ");
+            }
+
+            header("Location: pProfile.php");
+    } else {
+        echo '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                    <strong>Oops! </strong>
+            </div>';
+    }
+
+    
+}
 
 ?>
 <!DOCTYPE html>
@@ -41,215 +95,182 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Car Tech - Profile</title>
+    <title>Car Tech - Edit Profile</title>
     <!-- my css files -->    
     <link rel="icon" href="../imgs/icon.png" type="image/icon type">
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/animate.css">
     <link rel="stylesheet" href="../css/style.css" />
-    <link rel="stylesheet" href="../css/pProfile.css" />
+    <!-- <link rel="stylesheet" href="../css/pProfile.css" /> -->
+    <link rel="stylesheet" href="../css/EditPProfile.css" />
 
 </head>
 <body>
-    
     <?php include('../header.php'); ?>
-
-    <div class="prof-section">
-        <div class="container">
-            <div class="upper-prof row">
-                <div class="wow wobble img-prof col-3">
-                   
-                    <div class='imgProf'>
-                        <?php
-                            $background_colors = array('#282E33', '#25373A', '#164852', '#495E67', '#FF3838');
-                            $rand_background = $background_colors[array_rand($background_colors)];
-                            
-                            echo"<h2 style='background-color: ". $rand_background .";'>";
-                            echo strtoupper(substr($user_data['username'], 0, 1)) ;
-                            echo "</h2>";
-                        ?> 
-                    </div>
-
-                </div>
-                
-                
-                <div class="col-9 contact-info">
-                    <div class="account-name">
-                        <h2>
-                            <?php 
-                                echo $user_data['username'];
-                            ?>
-                        </h2><hr>
-                    </div>
-                    <div class="phone">
-                        <i class="fa fa-phone-square"></i>
-                        <span>
-                            <?php 
-                                echo $user_data['phone'];
-                            ?>
-                        </span>
-                    </div>
-                    <div class="mail">
-                        <i class="fa fa-envelope-square"></i>
-                        <span>
-                            <?php 
-                                echo $user_data['email'];
-                            ?>
-                        </span>
-                    </div>
-                    <div class="adress">
-                        <i class="fa fa-address-book"></i>
-                        <span> <?php
-                            echo " " . $user_data['street'] ;
-                            echo ",";
-                            echo " " . $user_data['region_name'] . " ";
-                            echo ",";
-                            echo " " . $user_data['city_name'] . " ";
-                        ?>  
-                        </span>
-                        <?php
-                        if($user_data['account_type'] == 'Provider') {
-                        ?>  
-                            <button class="addloc btn btn-outline-info" style="right: 130px" onclick="getLocation()">
-                            <?php 
-                                $cords="SELECT * FROM coordinates WHERE p_id = '$id' ";
-                                $ifcords = mysqli_query($conn, $cords);
-                                if($ex = mysqli_num_rows($ifcords)) {echo "update Location";} 
-                                else {echo "Add Location";}
-                            ?>
-                            </button>
-
-                            <button class="btn btn-outline-info" onclick="window.location.href ='EditPProfile.php';">Edit Info</button>
-                        <?php
-                        }
-                        ?>
-
-                    </div>
-                </div>
-
-                <div class="services-prof">
-                    <h3>Favorite list</h3>
-                    <button class="btn btn-outline-info" onclick="window.location.href ='favoriteUser.php';">
-                        Show Favorite 
-                    </button>
-               
-                    
-                </div>
-                <div class="services-prof">
-                    <h3>Services</h3>
-                    <ul>
-                        <?php
-                        $sql = "SELECT * FROM prov_services WHERE p_id = '$id'";
-                        $res = mysqli_query($conn, $sql);
-                        
-                        // echo"befor whil 1";
-                        while($ser_id = mysqli_fetch_array($res))
-                        {
-                            $Sql_ser="SELECT * FROM services WHERE id = ".$ser_id['ser_id']." ";
-                            $res_Ser = mysqli_query($conn, $Sql_ser);
-                            while($ser_Name = mysqli_fetch_array($res_Ser))
-                            { 
-                            // print_r($ser_Name);
-                            // echo"befor whil 2";
-                            echo "<li>".$ser_Name['ser_name']."</li>";
-                            // echo"befor ser 2";
-                            }
-                        }
-                        
-                        ?>
-                    </ul>
-                </div>
-                <div class="work-gallery col-12">
-                    <form method="POST" enctype="multipart/form-data" action="Gallery.php">
-                        
-                        <div class="inForm">
-                            <h3>Work Gallery</h3>
-                            <hr>
-                            <p> Your Description </p>
-                            <p>
-                                <textarea name="MSG"  cols="45" rows="5" placeholder="Enter description about this photo" required></textarea>
-                            </p>
-                            <p> Image </p>
-                            <p>
-                                <input type="file" name="G_image" accept="image/*" required class="file" >
-                            </p>
-                            <p>
-                                <input type='submit' name='submit' value ='Add New' class="btn btn-outline-info newBtn" >
-                            </p>
-                        </div>
-                        
-                    </form>
-                        
-                    <?php
-                        $sql_G= "SELECT * FROM gallery WHERE P_id='$id'";
-                        $result_G = mysqli_query($conn, $sql_G); 
-                        $_rows = mysqli_num_rows($result_G);
-                        if($_rows >= 1) {
-                        while ($user_G = mysqli_fetch_array($result_G))
-                            {
-                        ?>
-                            <div class="card" style="width: 19%;">
-                                <div class="card-img-top" >
-                                    <img width="100%" src="data:image/jpg;charset=utf8mb4;base64,<?php echo base64_encode($user_G['G_image']); ?> " />
-                                </div>    
-                                    <div class="card-body" >
-                                    <?php 
-                                        echo"<p class='card-text'>". $user_G['MSG']."</p>";
-                                        echo "<a style='float:right; padding-bottom:10px;' href='Delete.php?id=".$user_G['id']."'><img src='../imgs/Delete.png' width='20px'> </a>";                                    
-                                    ?>
-                                    </div>
-                            </div>
-                            <?php   
-                            }
-                        }
-                    ?>  
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script>
-		//var x = document.getElementById("demo");
-		var longitudeCoords=0;
-		var latitudeCoords=0;
-		
-		function getLocation() {
-			//if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(showPosition);
-			/*} else { 
-				x.innerHTML = "Geolocation is not supported by this browser.";
-			}*/
-		}
-
-		function showPosition(position) {
-				console.log(position, "position")
-		
-			longitudeCoords=position.coords.longitude; //30
-			latitudeCoords=position.coords.latitude; //5505
-			
-            var u = "./get_loc.php?lon=" + longitudeCoords+"&lat="+latitudeCoords;
-            var x = window.open(u,'_self');
-		}
-		
-		function goPosition() {
-			if (longitudeCoords==0 || latitudeCoords==0)
-				getLocation();
-			var url = "http://maps.google.com/maps?q="+ latitudeCoords + 
-			",+" +longitudeCoords ;
-			
-			location.replace(url);
-        }
-	</script>
     
+
+    <div class="container edit">
+           
+            <form  class="row" name="update_user" method="post" >
+                <div class="col-12 name">
+                    <label style="color:#08526d; font-weight:500;">Name</label><br>
+                    <input type="text" class="inputStyle" name="username" value="<?php echo $user_data['username']; ?>"  required><br>
+                </div>
+                <div class="col-12 row email-pass">
+                    <div class="col-6">
+                        <label style="color:#08526d; font-weight:500;">Email</label><br>
+                        <input type="Email" class="inputStyle" name="email" value="<?php echo $user_data['email']; ?>" required><br>
+                    </div>
+                    <div class="col-6">
+                        <label style="color:#08526d; font-weight:500;">Password</label><br>
+                        <input type="password" class="inputStyle" name="pass" id="myInput" value="<?php echo $user_data['password']; ?>" required>
+                        <img src="../imgs/eye-slash-512.png" width="20px" onclick=" myFunction();" style="cursor: pointer;" /><br>
+                    </div>
+                </div>
+                <div class="col-12 row address-phone">
+                    <div class="col-6">
+                        <label style="color:#08526d; font-weight:500;">Phone Number</label><br>
+                        <input class="inputStyle" type="number" name="phone" value="<?php echo $user_data['phone']; ?>" required> <br>
+
+                    </div>
+                    <div class="col-6">
+                        <div class="last-add">
+                            <label style="color:#08526d; font-weight:500;">Address</label><br>
+                            <span> 
+                            <?php
+                                echo " " . $user_data['street'] ;
+                                echo ",";
+                                echo " " . $user_data['region_name'] . " ";
+                                echo ",";
+                                echo " " . $user_data['city_name'] . " ";
+                            ?>  
+                            </span><br>
+                            <button id='chang'>Change</button>
+                        </div>
+                        <div  class="last-add">
+                            <label style="color:#08526d; font-weight:500;">Address</label><br>
+                            <div class="city">
+                                <label>City</label>
+                                <?php
+                                    include('search_citis.php'); 
+                                ?>
+                            </div>
+                            <div class="region">
+                                <label>Region</label>
+                                <select id="Region1" name="Region" class="search-select">
+                                    <option value="none" selected>Choose ...</option>
+                                </select>
+                            </div>
+
+                            <div class="street">
+                                <label for="">Street</label>
+                                <input type="text" name="street" placeholder="street" style="margin-left: 45px;">
+                            </div>
+
+                            <div class="ok">
+                                <span style="width:110%">Ok</span>
+                            </div>
+                            <button id='close'>x</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 row">
+
+                    <div class="col-8 services">
+                        <label style="color:#08526d; font-weight:500;">Services</label><br>
+                            <?php
+                            $sql = "SELECT 
+                                prov_services.ser_id,
+                                services.ser_name  
+                                FROM prov_services
+                                INNER join services on services.id = prov_services.ser_id
+                                WHERE prov_services.p_id = '$id'";
+                                
+                            $res = mysqli_query($conn, $sql);
+                            $arr = array( "Gas" => 'Gas Station',
+                                            "Wash" => 'Car Wash',
+                                            "Maintenance" => 'Car Maintenance',
+                                            "Trailer" => 'Trailer Truck');
+                            $stack = array();
+                            while($ser_id = mysqli_fetch_array($res))
+                            { 
+                                array_push($stack, $ser_id['ser_name']);
+                            }
+                            foreach ($arr as $k => $v )
+                            {
+                                if(in_array($v , $stack) ) {
+                                    echo "<input type='checkbox' name='".$k."' checked>";
+                                    echo "<label class='service'>". $v ."</label>";
+                                } 
+                                else {
+                                    echo "<input type='checkbox' name='".$k."'>";
+                                    echo "<label class='service'>". $v ."</label>";
+                                }
+                            }
+                            
+                            ?>
+                    </div>
+
+                    <div class="col-4 submit">
+                        <input type="submit" class="btn btn-outline-info" name="update" value="Update"><br>
+                    </div>
+
+                </div>
+            </form>
+    </div>
+    <div style="clear:both;"></div>
     
     <?php include('../footer.php'); ?>
+
     
     <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="../js/Password.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/wow.min.js"></script>
     <script>new WOW().init();</script>    
     <script src="../js/script.js"></script>
+
+    <script>
+        $('button').click(function(e) {
+            e.preventDefault();
+            return false;
+        });
+
+        $("#chang").click(function(){
+            $(".last-add:first-child").css("display", "none");
+            $(".last-add:last-child").css("display", "block");
+        });
+
+        $("#close").click(function(){
+            $(".last-add:first-child").css("display", "block");
+            $(".last-add:last-child").css("display", "none");
+        });
+
+        $(".ok span").click(function() {
+
+            if(($(".region option:selected").val() != "none") && ($(".street input").val() != ' ') && ($(".street input").val() != '')) {
+                var id = "<?php echo $id; ?>";
+                var reg = $(".region option:selected").val();
+                var street = $(".street input").val();
+
+                var new_address = street+ ", " + $(".region option:selected").text() + ", " + $(".city option:selected").text();
+                $(".last-add:first-child span").text(new_address);
+                
+                $.ajax({
+                    url: "EditPProfile.php",
+                    method: "POST",
+                    data: { id: id, reg: reg, street: street },
+                    success: function () {
+                    }
+                })
+
+                $(".last-add:first-child").css("display", "block");
+                $(".last-add:last-child").css("display", "none");
+            }
+        });
+
+    </script>
+
 </body>
 </html>
-
