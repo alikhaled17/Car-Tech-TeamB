d70c606380755601c064b4e80373bddd74531e52
alikhaled17 <ka99ali@gmail.com>
2021-01-12 09:34:27 +0200

    fix header color

--- src/logoutU.php
+++ src/logoutU.php
@@ -1,6 +1,10 @@
 <?php
+   include('../Config.php');  
    session_start();
+   $lg = $_SESSION['login_details_id'];
+   $result=mysqli_query($conn,"DELETE FROM  login_details WHERE login_details_id = $lg ");
    unset($_SESSION['u_id']);
+
    session_destroy();
    header("Location:/Car-Tech-TeamB/index.php");
 ?>
